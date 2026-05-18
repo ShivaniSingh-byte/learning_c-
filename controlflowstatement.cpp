679facc8dc0e7b8c60f1dc15a1f35f75d9a43164
Shivani Singh-byte <shivamsinghsengar400@gmail.com>
2026-05-18 08:12:01 +0530

    control_flow_statement file added

---
+++ controlflowstatement.cpp
@@ -0,0 +1,252 @@
+#include<iostream>
+using namespace std;
+int main(){
+     /* int a;
+      cout<<"Enter 1 , 2, 3, 4 or 5 :";
+      cin>>a;
+
+      if(a==1){
+        cout<<"enter 4:";
+        cin>>a;
+
+         if(a==4){
+            cout<<"Reached";
+        }
+          else{
+            cout<<"Invalid";
+          }
+      }
+
+      else if(a==2){
+        cout<<"Enter 1,2 or 4 :";
+        cin>>a;
+
+        if(a==1){
+            cout<<"Enter 2, 4 :";
+            cin>>a;
+        }
+
+       else if(a==2){
+            cout<<"Enter 4:";
+            cin>>a;
+            cout<<"Reached";
+        }
+
+          else if(a==4){
+              cout<<"Reached";
+          }
+
+            else {
+                cout<<"Invalid";
+            }
+      }
+
+
+      else if(a==3){
+        cout<<"Enter 1,3 or 4:";
+        cin>>a;
+
+        if(a==1){
+            cout<<"Enter 3,4:";
+            cin>>a;
+        }
+
+        else if(a==3){
+                cout<<"Enter 4:";
+                cin>>a;
+                cout<<"Reached";
+            }
+
+              //else if
+
+            else {
+                cout<<"Invalid";
+            }
+      }
+
+
+      else if(a==4){
+        cout<<"Enter 1,2,3,4 :";
+        cin>>a;
+
+        if(a==1){
+            cout<<"Enter 1:";
+            cin>>a;
+        }
+
+        else if(a==2){
+            cout<<"Enter 2 :";
+            cin>>a;
+        }
+
+        else if(a==3){
+            cout<<"Enter 3:";
+            cin>>a;
+        }
+        else if (a==4){
+            cout<<"Enter 4:";
+            cin>>a;
+            cout<<"Reached";
+        }
+        else{
+            cout<<"Invalid";
+        }
+      }
+
+       else if(a==5){
+        cout<<"Enter 1,2,3 or 4:";
+        cin>>a;
+
+         if(a==1){
+
+         }
+       }
+
+
+
+      else{
+        cout<<"Invalid";
+      }
+
+         /*  FLIPKART
+       string website;
+       cout<<"Enter website name :";
+       cin>>website;
+
+       if(website=="flipkart"){
+        string ps;
+        cout<<"Enter product or service :";
+        cin>>ps;
+
+        if(ps=="product"){
+            string p;
+            cout<<"Enter mobile or laptop :";
+            cin>>p;
+
+        if(p=="mobile"){
+            cout<<"20000";
+        }
+        else if (p=="laptop"){
+            cout<<"50000";
+        }
+        else {
+            cout<<"invalid";
+        }
+       }
+
+       else if (ps=="service"){
+            cout<<"not available";
+       }
+       else {
+        cout<<"invalid";
+       }
+       }
+
+       else {
+        cout<<"Invalid Name";
+       }*/
+
+         /*AMAZON
+      string website;
+      cout<<"Enter website name :";
+      cin>>website;
+
+      if(website=="amazon"){
+        string ps;
+        cout<<"enter product or service :";
+        cin>>ps;
+
+        if(ps=="product"){
+            string p;
+            cout<<"Enter shoes or clothes :";
+            cin>>p;
+
+        if(p=="shoes"){
+            cout<<"1000";
+        }
+        else if(p=="clothes"){
+            cout<<"2000";
+        }
+        else {
+            cout<<"Invalid";
+        }
+      }
+
+      else if(ps=="service"){
+            cout<<"Not Available";
+      }
+
+
+      else {
+        cout<<"Invalid";
+      }
+     }
+
+      else{
+        cout<<"Invalid Name";
+      }*/
+
+
+
+      /*Courses--
+      int courses;
+      cout<<"1 is fullstack python\t 2 is fullstack java\t 3 is mern :\n";
+      cin>>courses;
+      if(courses==1){
+        int a;
+        cout<<"1. 5+1 & 2. 6+2 :";
+        cin>>a;
+        if(a==1){
+            cout<<40000;}
+        else if(a==2){
+            cout<<50000;}
+      }
+      else if(courses==2){
+        int b;
+        cout<<"1. 5+1 & 2. 6+2 :";
+        cin>>b;
+        if(b==1){
+            cout<<45000;}
+        else if(b==2){
+            cout<<55000;}
+      }
+      else if(courses==3){
+        int c;
+        cout<<"1. 5+1 & 2. 6+2 :";
+        cin>>c;
+        if(c==1){
+            cout<<20000;}
+        else if(c==2){
+            cout<<30000;}
+      }
+      else{
+        cout<<"Invalid Input";
+      }
+
+      /*ATM--click 1 for widthdrawl 2 for deposite 3 for balance_cheaque:
+      int atm,balance=10000;
+      cout<<"1 for withdrawl\t";
+      cout<<"2 for deposite\t";
+      cout<<"3 for balance_cheaque\n";
+      cin>>atm;
+      if(atm==1){
+        int amount_withdrawl;
+        cout<<"Enter Amount :";
+        cin>>amount_withdrawl;
+        cout<<balance-amount_withdrawl;
+      }
+      else if(atm==2){
+        int amount_deposite;
+        cout<<"Enter Amount :";
+        cin>>amount_deposite;
+        cout<<balance+amount_deposite;
+      }
+      else if(atm==3){
+        cout<<balance;
+      }
+      else{
+        cout<<"Invalid Input";
+      }*/
+
+
+}
