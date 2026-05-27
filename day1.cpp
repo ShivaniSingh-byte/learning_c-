67975ae1f0e8f73d3c0bbc2e4597e182ee7f7335
Shivani Singh-byte <shivamsinghsengar400@gmail.com>
2026-05-27 09:16:29 +0530

    day1 practice file added

---
+++ day1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    1. Write a program to print “Hello, World!” on the screen.
+    cout<<"Hello World";
+
+    2. Write a program to print your name, roll number, and course.
+    string SHIVANIname;
+    string SHIVANIroll;
+    string SHIVANIcourse;
+    cout<<"Enter your name :"<<endl;
+    cin>>SHIVANIname;
+    cout<<"Enter rollname :"<<endl;
+    cin>>SHIVANIroll;
+    cout<<"Enter your course :"<<endl;
+    cin>>SHIVANIcourse;
+
+    3. Write a program to add two numbers entered by the user.
+    int SHIVANIa;
+    int SHIVANIb;
+    cout<<"Enter 1st no:";
+    cin>>SHIVANIa;
+    cout<<"Enter 2nd no:";
+    cin>>SHIVANIb;
+    cout<<"additon :"<<SHIVANIa+SHIVANIb;
+}
