cc2825b651fcb144b7d4cd0eb2f07ccb27864fe8
Shivani Singh-byte <shivamsinghsengar400@gmail.com>
2026-06-17 12:00:45 +0530

    revision file added

---
+++ revision.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+using namespace std;
+int main(){
+   /* LOOP :
+     * do-while / exit loop:
+        static program:
+    int a=1;
+    do
+    {
+        cout<<a<<" ";
+        a=a+1; //a++;
+    }while(a<=5);
+
+        dynamic program:
+    int start,ends;
+    cout<<"Enter start value\n";
+    cin>>start;
+    cout<<"Enter ends value\n";
+    cin>>ends;
+
+    do
+    {
+       cout<<start;
+       start=start+1;
+    }while(start<=ends);
+
+     * while / entry loop: uncertanity
+     static program:
+    int a=1;
+    while(a<=5){
+        cout<<a<<" ";
+        a=a+1;
+    }
+
+    increase the no is +1:
+    int a=1;
+    while(a)
+    {
+         cout<<a;
+         a=a+1;
+    }
+
+    blank output
+    int a=1;
+    while(a);
+    {
+         cout<<a;
+         a=a+1;
+    }
+
+    if condition :
+    int a=1;
+    if(a<=10);
+    {
+         cout<<a;
+    }
+
+    int a=1;
+    if(a<=10);
+    cout<<a;
+
+    * for loop / inline statement loop : certanity
+    Line of coding is reduced
+    for(int a=1; a<=5; a=a+1)
+    {
+        cout<<a<<" ";
+    }
+
+    for(int a=5; a>=1; a=a-1)
+    {
+        cout<<a<<" ";
+    }
+
+    ODD NO:
+    for(int a=1; a<=10; a=a+1)
+    {
+        if(a%2!=0){
+        cout<<a<<" ";
+        }
+    }
+
+    for(int a=1; a<=10; a=a+2)
+    {
+        cout<<a<<" ";
+    }*/
+
+}
