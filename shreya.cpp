82555af1ee9cdff3defddae57f8490b54729f695
Shivani Singh-byte <shivamsinghsengar400@gmail.com>
2026-06-19 16:34:24 +0530

    shreya file added

---
+++ shreya.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    /*int a;
+    cout<<"enter the no..:";
+    cin>>a;
+    (a%8==0)?cout<<"multiple":cout<<"not multiple";
+    int a;
+    cout<<"enter the no.. :";
+    cin>>a;
+    (a>=50 && a<=100)?cout<<"inbetween":cout<<"not inbetween";
+    int a;
+    cout<<"enter the no.. :";
+    cin>>a;
+    (a>=33)?cout<<"pass":cout<<"fail";
+    int year;
+    cout<<"enter the year";
+    cin>>year;
+    (year%400==0)&&(year%4==0)||(year%100!=0)?cout<<"leap year":cout<<"not leap year";
+    int a;
+    cout<<"enter the no.. :";
+    cin>>a;
+    (a>=0)?cout<<"positive":cout<<"negative";
+    int a;
+    cout<<"enter the no :";
+    cin>>a;
+    (a>=50 && a%4==0)?cout<<"":cout<<"";
+    char ch;
+    cout<<"enter a word :";
+    cin>>ch;
+    (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')?cout<<"vowel":cout<<"constant";
+    int a;
+    cout<<"enter the no :";
+    cin>>a;
+    (a%2==0 && a%4==0)?cout<<"divisible":cout<<"not divisible";
+    int age;
+    cout<<"enter ur age :";
+    cin>>age;
+    (age>=0)?cout<<"vote":cout<<"not vote";
+    int a;
+    cout<<"enter the no :";
+    cin>>a;
+    (a>=50)?cout<<"above":cout<<"not above";*/
+
+}
