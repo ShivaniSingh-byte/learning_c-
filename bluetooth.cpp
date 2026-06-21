96197f83cb020a580251a505411d2338a65ac809
Shivani Singh-byte <shivamsinghsengar400@gmail.com>
2026-06-21 11:17:54 +0530

    bluetooth file added

---
+++ bluetooth.cpp
@@ -0,0 +1,124 @@
+#include<iostream>
+//#include <cmath>
+using namespace std;
+int main(){
+    /*Print all Armstrong numbers within a range.
+    int start, end;
+    cout << "Enter start of range: ";
+    cin >> start;
+    cout << "Enter end of range: ";
+    cin >> end;
+
+    for (int num = start; num <= end; num++) {
+        int temp = num, sum = 0;
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += digit * digit * digit;
+            temp /= 10;
+        }
+
+        if (sum == num) {
+            cout << num << " ";
+        }
+    }
+
+    Print all prime numbers within a given range.
+    int start, end;
+    cout << "Enter start of range: ";
+    cin >> start;
+    cout << "Enter end of range: ";
+    cin >> end;
+
+    for (int num = start; num <= end; num++) {
+        int count = 0;
+
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+
+        if (count == 2) {
+            cout << num << " ";
+        }
+    }
+
+    Implement a program to find all numbers divisible by 3 and 5 within a range.
+    int start, end;
+    cout << "Enter the starting number of the range: ";
+    cin >> start;
+    cout << "Enter the ending number of the range: ";
+    cin >> end;
+
+    for (int i = start; i <= end; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+
+    Write a program to calculate the sum of squares of the first N natural numbers.
+    int N;
+    int sum = 0;
+
+    cout << "Enter a positive integer N: ";
+    cin >> N;
+
+        for (int i = 1; i <= N; i++) {
+            sum += i * i;  // add square of i
+        }
+        cout << "Sum of squares of first " << N
+             << " natural numbers = " << sum << endl;
+
+    inverted right :
+    for(int row=1; row<=5; row++){
+        for(int col=1; col<=5; col++){
+            if(col<=6-row){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+
+    left :
+    for(int row=1; row<=5; row++){
+        for(int col=1;col<=5;col++){
+            if(col>=6-row){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+
+    inverted left :
+    for(int row=1; row<=5; row++){
+        for(int col=1; col<=5; col++){
+            if(col>=row){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+
+    right:
+    for(int row=1; row<=5; row++){
+        for(int col=1; col<=5;col++){
+            if(col<=row){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }*/
+}
